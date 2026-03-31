deaa82771dbd3d9afb8e451d33ba4fd9d6ea4709
Vitor Galletti <vitorgalletti@hotmail.com>
2026-03-31 16:19:30 -0300

    Tira a responsabilidade do meu model e passa para dentro do meu DTO, usando streams no mapper

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaService.java
@@ -17,14 +17,18 @@ public class NinjaService {
     }
 
     // Listar todos os meus ninjas
-    public List<NinjaModel> listarNinjas() {
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listarNinjas() {
+        List<NinjaModel>  ninjas = ninjaRepository.findAll();
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
+
     }
 
     // Listar todos os meus ninjas por ID
-    public NinjaModel listarNinjasPorId(Long id) {
+    public NinjaDTO listarNinjasPorId(Long id) {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
-        return ninjaPorId.orElse(null);
+        return ninjaPorId.map(ninjaMapper::map).orElse(null);
     }
 
     // Criar um novo Ninja
@@ -40,12 +44,16 @@ public class NinjaService {
     }
 
     // Atualizar ninja
-    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado) {
-        if (ninjaRepository.existsById(id)) {
+    public NinjaDTO atualizarNinja(Long id, NinjaDTO ninjaDTO) {
+        Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id);
+        if (ninjaExistente.isPresent()) {
+            NinjaModel ninjaAtualizado = ninjaMapper.map(ninjaDTO);
             ninjaAtualizado.setId(id);
-            return ninjaRepository.save(ninjaAtualizado);
+            NinjaModel ninjaSalvo = ninjaRepository.save(ninjaAtualizado);
+            return ninjaMapper.map(ninjaSalvo);
         }
         return null;
+
     }
 
 }
