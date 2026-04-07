f1439e8172883a3021cc9652a91af5590999c1d1
Vitor Galletti <vitorgalletti@hotmail.com>
2026-04-07 16:45:54 -0300

    feature: adicionando frontend na minha API

---
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaControllerUi.java
@@ -0,0 +1,28 @@
+package dev.java10x.CadastroDeNinja.Ninjas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas(Model  model) {
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "lista-ninjas";
+    }
+
+
+}
